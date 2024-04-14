88f3806948f99f78a250e3adef2c31a2b5b789b3
PaulJFS <paul.jf.sieber@icloud.com>
2024-04-14 14:34:26 +0200

    final commit

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -2,7 +2,10 @@ import { ReactComponent as Logo } from "./cons Logo.svg";
 import Pb from "./IMG_6490.PNG";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
+import { CardBody, CardContainer, CardItem } from "./components/ui/3d-card";
+import Leipzig from "./assets/lpz.jpeg";
+import Bremen from "./assets/Bmn.jpeg";
+import München from "./assets/muc.jpeg";
 function LandingPage() {
   const [location, setLocation] = useState(() => {
     const savedLocation = localStorage.getItem("selectedLocation");
@@ -35,6 +38,30 @@ function LandingPage() {
     localStorage.setItem("selectedLocation", selectedLocation);
   };
 
+  let src = Leipzig;
+  let route = "";
+
+  switch (location) {
+    case "Bremen":
+      src = Bremen;
+      route =
+        "https://www.google.de/maps/place/Bremen/@53.1199176,8.5714818,11z/data=!3m1!4b1!4m6!3m5!1s0x47b128100ceee335:0xc2e8885cf937df51!8m2!3d53.0792962!4d8.8016936!16zL20vMDE3d18?entry=ttu";
+      break;
+    case "Leipzig":
+      src = Leipzig;
+      route =
+        "https://www.google.de/maps/place/Leipzig/@51.3416669,12.2288305,11z/data=!3m1!4b1!4m6!3m5!1s0x47a6f818200f2c73:0x93df80d2b9b4f552!8m2!3d51.3396955!4d12.3730747!16zL20vMDRrZjQ?entry=ttu";
+      break;
+    case "München":
+      src = München;
+      route =
+        "https://www.google.de/maps/place/M%C3%BCnchen/@48.154793,11.3770307,11z/data=!3m1!4b1!4m6!3m5!1s0x479e75f9a38c5fd9:0x10cb84a7db1987d!8m2!3d48.1351253!4d11.5819805!16s%2Fm%2F02h6_6p?entry=ttu";
+      break;
+    default:
+      src = Leipzig;
+      route = "";
+  }
+
   return (
     <>
       <header className="bg-regal-blue p-5 pl-5 border-none lg:pl-32 sticky top-0">
@@ -50,19 +77,24 @@ function LandingPage() {
             <option value="Leipzig">Leipzig</option>
           </select>
         </div>
-        <ul className="text-regal-white flex gap-5 mt-5 font-bold">
-          <li>Lorem</li>
-          <li>Ipsum</li>
-          <li>Dolor</li>
+        <ul className="text-regal-white flex gap-2 mt-3 font-bold">
+          <li className="px-2 py-2 rounded-md hover:bg-regal-green/90">
+            Lorem
+          </li>
+          <li className="px-2 py-2 rounded-md hover:bg-regal-green/90">
+            Ipsum
+          </li>
+          <li className="px-2 py-2 rounded-md hover:bg-regal-green/90">
+            Dolor
+          </li>
         </ul>
       </header>
       <div className="bg-regal-blue text-regal-white border-none mx-auto flex flex-col lg:flex-row items-start justify-center gap-16 lg:gap-32 px-8 py-12 lg:py-32">
-        <h1 className="text-4xl max-w-sm text-regal-green text-left">
-          <span className="font-bold">Lorem ipsum dolor sit amet,</span>{" "}
-          consetetur sadipscing elitr.
+        <h1 className=" text-4xl max-w-sm text-regal-green text-left justify-items-start">
+          <b>Lorem ipsum dolor sit amet,</b> consetetur sadipscing elitr.
         </h1>
         <img
-          className="rounded-md w-full lg:w-1/3 px-5 h-auto "
+          className="rounded-md w-full lg:w-1/3    "
           alt="profile-pic"
           src={Pb}
         />
@@ -70,8 +102,7 @@ function LandingPage() {
       <div className="flex flex-col items-center mt-10">
         <div className="max-w-2xl text-left flex flex-col gap-10 mb-20 px-5">
           <h1 className="text-4xl  text-regal-green text-left">
-            <span className="font-bold">Lorem ipsum dolor sit amet,</span>{" "}
-            consetetur sadipscing elitr.
+            <b>Lorem ipsum dolor sit amet,</b> consetetur sadipscing elitr.
           </h1>
           <p>
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
@@ -85,19 +116,22 @@ function LandingPage() {
             gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
           </p>
         </div>
-        <div className="flex flex-col lg:flex-row gap-3 mx-5 mb-10 max-w-6xl justify-items-center">
-          <div className=" bg-regal-green text-regal-white text-xl rounded-md p-5 text-left w-full min-h-64">
+        <div className="flex flex-col lg:flex-row gap-3 mx-5 mb-10 max-w-6xl justify-center">
+          <div className=" bg-regal-green text-regal-white text-2xl rounded-md p-5 text-left w-full min-h-64">
             {weather && (
               <>
                 <p>
-                  Die aktuelle Temperatur in {location} beträgt{" "}
-                  {weather.current.temp_c}°C und das Wetter ist{" "}
-                  {weather.current.condition.text}.
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                  diam nonumy eirmod tempor invidunt ut labore{" "}
+                  <b> et dolore magna aliquyam erat, sed diam voluptua.</b> At
+                  vero eos et accusam et justo duo dolores et ea rebum. Stet
+                  clita kasd gubergren, no sea takimata sanctus est.
                 </p>
               </>
             )}
           </div>
-          <div className=" bg-regal-blue rounded-md p-5 flex flex-col gap-2">
+
+          <div className=" bg-regal-blue rounded-md p-5 flex flex-col gap-2 w-full">
             <h1 className="text-3xl lg:text-4xl text-regal-green text-left mb-5">
               <span className="font-bold">Lorem ipsum dolor sit amet,</span>{" "}
               consetetur sadipscing elitr.
@@ -112,19 +146,99 @@ function LandingPage() {
               placeholder="Nachricht"
               rows="5"
             />
-            <div className=" text-regal-white">
+            <div className=" text-regal-white flex justify-left items-center gap-1">
               <input
                 className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"
                 type="checkbox"
               />{" "}
               Einwilligung zur Datenverarbeitung*
             </div>
-            <button className="rounded-md bg-regal-orange text-regal-white font-bold p-2">
+            <button className="rounded-md md:w-40 bg-regal-orange text-regal-white font-bold p-2">
               Jetzt absenden!
             </button>
           </div>
         </div>
       </div>
+      <CardContainer className=" inter-var mb-10">
+        <CardBody className=" group/card bg-regal-blue text-regal-green rounded-md p-5 text-left ">
+          <CardItem
+            translateZ="50"
+            className="text-4xl font-bold text-regal-green hover:scale-125"
+          >
+            <div className="flex flex-row items-center gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-8 h-8"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+                />
+              </svg>
+              <span>
+                {location} {weather.current.temp_c}°C
+              </span>
+            </div>
+          </CardItem>
+          <CardItem
+            as="p"
+            translateZ="60"
+            className="text-neutral-200 text-sm max-w-sm mt-2"
+          >
+            Das Wetter in {location} ist momentan{" "}
+            {weather.current.condition.text}.
+          </CardItem>
+          <CardItem translateZ="100" className="w-full mt-4">
+            <img className="rounded-md " alt="city-pic" src={src} />
+          </CardItem>
+          <div className="flex justify-between items-center mt-6">
+            <CardItem
+              translateZ="20"
+              as="a"
+              href={route}
+              target="__blank"
+              className="px-4 py-2 rounded-xl text-xs font-normal "
+            >
+              Route →
+            </CardItem>
+            <CardItem
+              translateZ="20"
+              as="a"
+              href="https://github.com/PaulJFS/Frontend-Test"
+              target="__blank"
+              className="px-4 py-2 rounded-xl bg-regal-green text-white  text-xs font-bold"
+            >
+              <div className="flex flex-row items-center gap-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-4 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                </svg>
+                GitHub
+              </div>
+            </CardItem>
+          </div>
+        </CardBody>
+      </CardContainer>
     </>
   );
 }
